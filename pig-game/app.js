693cffe296d65700ff6d22afb93b66cea827ee9c
github username <danhjoo@gmail.com>
2020-03-27 15:55:58 -0700

    work on dice feature

--- pig-game/app.js
+++ pig-game/app.js
@@ -8,19 +8,48 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 */
 
-var scores, roundScore, activePlayer, dice;
+var scores, roundScore, activePlayer;
 
-scores = [0,0];
+scores = [0, 0];
 roundScore = 0;
-activePlayer = 1; 
-
-dice = Math.floor(Math.random() * 6) + 1;
-console.log(dice)
+activePlayer = 1;
 
 // document.querySelector('#current-' + activePlayer).textContent = dice
-document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
+// document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
 
-var x = document.querySelector('#score-0').textContent;
-console.log(x);
+// var x = document.querySelector('#score-0').textContent;
+// console.log(x)
 
 document.querySelector('.dice').style.display = 'none';
+
+document.getElementById('score-0').textContent = 0;
+document.getElementById('score-1').textContent = 0;
+document.getElementById('current-0').textContent = 0;
+document.getElementById('current-1').textContent = 0;
+
+
+
+document.querySelector('.btn-roll').addEventListener('click', function () {
+    //  1. random number needs to be generated
+    var dice = Math.floor(Math.random() * 6) + 1;
+
+    // 2. display the result 
+    var diceDOM = document.querySelector('.dice');
+    diceDOM.style.display = 'block';
+    diceDOM.src = 'dice-' + dice + '.png';
+
+    // 3. Update the round score only if the rolled number isn't a 1 
+    if (dice != 1) {
+        roundScore += dice;
+        document.querySelector('#current-' + activePlayer).textContent = roundScore;
+    } else {
+        // document.querySelector('#current-' + activePlayer).textContent = 0;
+        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+        roundScore = 0;
+
+        document.getElementById('current-0').textContent = '0'
+        document.getElementById('current-1').textContent = '0'
+
+        // document.querySelector('#current-' + activePlayer).textContent = 0;
+    }
+});
