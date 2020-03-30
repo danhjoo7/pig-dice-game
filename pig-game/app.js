6f9801c66dbdd738fb40feb08e758e33accadae8
github username <danhjoo@gmail.com>
2020-03-30 15:20:31 -0700

    add active ui

--- pig-game/app.js
+++ pig-game/app.js
@@ -12,7 +12,7 @@ var scores, roundScore, activePlayer;
 
 scores = [0, 0];
 roundScore = 0;
-activePlayer = 1;
+activePlayer = 0;
 
 // document.querySelector('#current-' + activePlayer).textContent = dice
 // document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
@@ -45,10 +45,13 @@ document.querySelector('.btn-roll').addEventListener('click', function () {
     } else {
         // document.querySelector('#current-' + activePlayer).textContent = 0;
         activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
-        roundScore = 0;
+        // roundScore = 0;
 
         document.getElementById('current-0').textContent = '0'
         document.getElementById('current-1').textContent = '0'
+
+        document.querySelector('.player-0-panel').classList.remove('active')
+        document.querySelector('.player-1-panel').classList.add('active')
 
         // document.querySelector('#current-' + activePlayer).textContent = 0;
     }
